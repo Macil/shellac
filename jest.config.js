827bd4c6902df5a6c818ea5d3d871d922951687d
Chris Cowan <agentme49@gmail.com>
2024-04-14 04:47:23 -0700

    update devdeps and make tests start running on Windows

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,4 @@
 export default {
   preset: 'ts-jest/presets/default-esm', // or other ESM presets
   testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
 }
